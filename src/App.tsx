4eeb9f5b106376b3826e05c806a2cad46d090e41
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-05-27 10:29:46 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from "react-native"
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Text>Teste</Text>
+        </SafeAreaView>
+    )
+};
+
+export default App;
